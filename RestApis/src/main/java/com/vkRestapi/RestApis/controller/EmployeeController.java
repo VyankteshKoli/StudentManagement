a498b67fd8d7de68064ae1833160ea80fa330003
Vyanktesh <141150425+VyankteshKoli@users.noreply.github.com>
2026-01-13 23:18:26 +0530

    Update EmployeeController.java

--- RestApis/src/main/java/com/vkRestapi/RestApis/controller/EmployeeController.java
+++ RestApis/src/main/java/com/vkRestapi/RestApis/controller/EmployeeController.java
@@ -17,32 +17,27 @@ public class EmployeeController {
         this.service = service;
     }
 
-    // CREATE
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
         return service.saveEmployee(employee);
     }
 
-    // READ ALL
     @GetMapping
     public List<Employee> getAllEmployees() {
         return service.getAllEmployees();
     }
 
-    // READ BY ID
     @GetMapping("/{id}")
     public Employee getEmployee(@PathVariable Long id) {
         return service.getEmployeeById(id);
     }
 
-    // UPDATE
     @PutMapping("/{id}")
     public Employee updateEmployee(@PathVariable Long id,
                                    @RequestBody Employee employee) {
         return service.updateEmployee(id, employee);
     }
 
-    // DELETE
     @DeleteMapping("/{id}")
     public String deleteEmployee(@PathVariable Long id) {
         service.deleteEmployee(id);
